9c4d886f0d89c36506c991cba2fbf637ab36eefe
josealarconchacon <jose.alarcon.chacon@gmail.com>
2025-09-30 21:43:16 -0400

    fix babel config

---
+++ jest.config.cjs
@@ -0,0 +1,27 @@
+module.exports = {
+  testEnvironment: "jsdom",
+  moduleFileExtensions: ["js", "jsx"],
+  transform: {
+    "^.+\\.jsx?$": "babel-jest",
+  },
+  setupFilesAfterEnv: ["@testing-library/jest-dom"],
+  testMatch: ["**/tests/**/*.test.js", "**/tests/**/*.test.jsx"],
+  collectCoverage: true,
+  collectCoverageFrom: [
+    "src/**/*.{js,jsx}",
+    "hooks/**/*.{js,jsx}",
+    "!src/main.jsx",
+    "!src/tests/**",
+    "!**/node_modules/**",
+  ],
+  coverageDirectory: "coverage",
+  coverageReporters: ["text", "lcov", "html", "json-summary"],
+  coverageThreshold: {
+    global: {
+      branches: 34,
+      functions: 66,
+      lines: 59,
+      statements: 60,
+    },
+  },
+};
